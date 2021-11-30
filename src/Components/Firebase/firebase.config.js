a876796f7205abac910a85b2e407b72747b4c00e
Ruhul Amin <ruhuliuice@gmail.com>
2021-11-30 08:56:54 +0600

    add authentication

--- src/Components/Firebase/firebase.config.js
+++ src/Components/Firebase/firebase.config.js
@@ -1,9 +1,16 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCbdrP048sZmE60tm3NawomZ87J7yogmZk",
-    authDomain: "react-firebase-4279d.firebaseapp.com",
-    projectId: "react-firebase-4279d",
-    storageBucket: "react-firebase-4279d.appspot.com",
-    messagingSenderId: "726729484561",
-    appId: "1:726729484561:web:e0e7a64eeecf96c50304b0"
-  };
-  export default firebaseConfig;
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+export default firebaseConfig;
+
+// apiKey: "AIzaSyBO6-gvLjC7YCqo4tKft5IfqvTrAM2WoWg",
+//   authDomain: "neuron-dental-care.firebaseapp.com",
+//   projectId: "neuron-dental-care",
+//   storageBucket: "neuron-dental-care.appspot.com",
+//   messagingSenderId: "611406306925",
+//   appId: "1:611406306925:web:c3944c9c2b09738b1ce964",
